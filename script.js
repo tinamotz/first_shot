91ff82ec9820008a90afb3c7de3ac51bc9479e2b
Tina Motz <tinamotz@me.com>
2015-06-18 21:01:57 -0500

    no message

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-	$('#flip').mouseenter(function() {
+	$('.flip').mouseenter(function() {
 		$(this).fadeOut('slow');
-		this.src = "images/*file*.jpg";
+		var original = this.src;
+		this.src = $(this).attr('othersrc');
+		$(this).attr('othersrc', original);
 		$(this).fadeIn('slow');
+	});
+	$('.flip').mouseleave(function() {
+		$(this).fadeOut('slow');
+		var original = $(this).attr('othersrc');
+		$(this).attr('othersrc', this.src);
+		this.src = original;
+		$(this).fadeIn('slow');	
 	});
 });
